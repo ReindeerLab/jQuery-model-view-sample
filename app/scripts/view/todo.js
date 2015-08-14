ed3e0177dcc40bf785a3fc8ad19c457aaac34edf
dachi023 <ar.19920223@gmail.com>
2015-08-14 16:55:16 +0900

    Add model and view

--- app/scripts/view/todo.js
+++ app/scripts/view/todo.js
@@ -1,2 +1,24 @@
-app.createClass('todoView', function() {
+$(function() {
+
+  var model, addRow;
+
+  model = new DataModel();
+
+  addRow = function(id, email, name) {
+    $('table tbody').append(
+      '<tr>'
+      + '<td>' + id + '</td>'
+      + '<td>' + email + '</td>'
+      + '<td>' + name + '</td>'
+      + '</tr>'
+    );
+  };
+
+  $('#add').on('click', function() {
+    var id, email, name;
+    email = $('#email').val();
+    name = $('#name').val();
+    id = model.add(email, name);
+    addRow(id, email, name);
+  });
 });
